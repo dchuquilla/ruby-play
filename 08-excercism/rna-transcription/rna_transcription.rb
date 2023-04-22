b46c62f52d3d38dd8482d5885be0d6d13bd07fdf
dchuquilla <dario.chuquilla@gmail.com>
2023-04-21 19:36:46 -0500

    🌟 Apply mentoring recommendations
    📝 `extend self` limits all module methods to class level, but only one
    method is required to it, the name of method is tr not TR.
    
    📊 List of changes made in bullet points:
    - 🌟 Enhancement:
      - Set of_dna as class level method by conveting to self.of_dna
      - change TR to tr in description of method
    
    🔗 Related Issues:
    - none
    
    👥 Reviewers:
    - kotp

--- 08-excercism/rna-transcription/rna_transcription.rb
+++ 08-excercism/rna-transcription/rna_transcription.rb
@@ -2,13 +2,11 @@
 
 # Generates RNA complement (per RNA transcription)
 module Complement
-  extend self # rubocop:disable Style/ModuleFunction
-
   DNA_NUCLEOTIDES = 'GCTA'
   RNA_NUCLEOTIDES = 'CGAU'
 
-  def of_dna(strand)
-    # more details about TR method in the following link
+  def self.of_dna(strand)
+    # more details about tr method in the following link
     # https://ruby-doc.org/3.2.2/String.html#method-i-tr
     strand.tr(DNA_NUCLEOTIDES, RNA_NUCLEOTIDES)
   end
