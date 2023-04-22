d1d09763cad80bf22cc249b459b554e823697cb5
dchuquilla <dario.chuquilla@gmail.com>
2023-04-21 22:44:22 -0500

    🐎 of_dna method as class << self and constants improvents
    📊 List of changes made in bullet points:
    - 🌟 Enhancement:
      - of_dna inside class << self block
      - constants as one line structure
    
    👥 Reviewers:
    - kotp

--- 08-excercism/rna-transcription/rna_transcription.rb
+++ 08-excercism/rna-transcription/rna_transcription.rb
@@ -2,12 +2,14 @@
 
 # Generates RNA complement (per RNA transcription)
 module Complement
-  DNA_NUCLEOTIDES = 'GCTA'
-  RNA_NUCLEOTIDES = 'CGAU'
 
-  def self.of_dna(strand)
-    # more details about tr method in the following link
-    # https://ruby-doc.org/3.2.2/String.html#method-i-tr
-    strand.tr(DNA_NUCLEOTIDES, RNA_NUCLEOTIDES)
+  DNA, RNA = 'GCTA', 'CGAU'
+
+  class << self
+    def of_dna(strand)
+      # more details about tr method in the following link
+      # https://ruby-doc.org/3.2.2/String.html#method-i-tr
+      strand.tr(DNA, RNA)
+    end
   end
 end
