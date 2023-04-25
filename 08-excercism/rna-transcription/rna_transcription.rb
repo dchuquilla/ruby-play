177b5e92e0380749b40222dab9b4825cfc5d4a4f
dchuquilla <dario.chuquilla@gmail.com>
2023-04-24 22:42:41 -0500

    🎭 Fixing line spaces and String#tr documentation reference.
    📊 List of changes made in bullet points:
    - 🌟 Enhancement:
      - Fix the the lack of a newline between the last method, the last
        class, and the definition of the module.
      - Use String#tr as reference of documentation.
    
    👥 Reviewers:
    - KOTP

--- 08-excercism/rna-transcription/rna_transcription.rb
+++ 08-excercism/rna-transcription/rna_transcription.rb
@@ -6,10 +6,13 @@ module Complement
   DNA, RNA = 'GCTA', 'CGAU'
 
   class << self
+
     def of_dna(strand)
-      # more details about tr method in the following link
-      # https://ruby-doc.org/3.2.2/String.html#method-i-tr
+      # see `String#tr` documentation for more information.
+      # https://ruby-doc.org/core-3.0.2/String.html#method-i-tr
       strand.tr(DNA, RNA)
     end
+
   end
+
 end
