89fb307a29c8817c96038411816b7289c83fb93c
dchuquilla <dario.chuquilla@gmail.com>
2023-05-20 17:36:53 -0500

    ✨ Reorganize code based on mentor's review
    📊 List of changes made in bullet points:
    - 🌟 Enhancement: [brief description of the enhancement made]
      - [ ] Mode calculate up to class level
      - [ ] Add commas to attr_readers
      - [ ] Simplify operand attributes foe beign shorter
      - [ ] Reduce operands type validate line by adding a custom error
            class and operand validations in its own method.
    
    👥 Reviewers:
    - @kotp

--- 08-excercism/simple-calculator/simple_calculator.rb
+++ 08-excercism/simple-calculator/simple_calculator.rb
@@ -3,40 +3,49 @@
 class SimpleCalculator
 
   ALLOWED_OPERATIONS = ['+', '/', '*']
-  DISPLAY = '%<first_operand>s %<operation>s %<second_operand>s = %<result>s'
-
-  private
-
-  def initialize(first_operand, second_operand, operation)
-    @first_operand = first_operand
-    @second_operand = second_operand
-    @operation = operation
-  end
-
-  public
-
-  attr_reader :first_operand
-              :second_operand
-              :operation
-
-  def to_s
-    raise ArgumentError.new('Operand type not supported') if !@first_operand.is_a?(Numeric) || !@second_operand.is_a?(Numeric)
-
-    raise UnsupportedOperation.new('Unsupported operation') unless ALLOWED_OPERATIONS.include?(@operation)
-
-    return 'Division by zero is not allowed.' if @second_operand.zero?
-
-    result = @first_operand.send(@operation, @second_operand)
-
-    DISPLAY % {first_operand: @first_operand, second_operand: @second_operand, operation: @operation,
-               result: result}
-  end
+  DISPLAY = '%<operand_1>s %<operation>s %<operand_2>s = %<result>s'
 
   def self.calculate(first_operand, second_operand, operation)
     new(first_operand, second_operand, operation).to_s
   end
 
+  private
+
+  def initialize(first_operand, second_operand, operation)
+    @operand_1 = first_operand
+    @operand_2 = second_operand
+    @operation = operation
+  end
+
+  public
+
+  attr_reader :operand_1,
+              :operand_2,
+              :operation
+
+  def valid_operands?
+    @operand_1.is_a?(Numeric) && @operand_2.is_a?(Numeric)
+  end
+
+  def to_s
+    raise OperandError unless valid_operands?
+
+    raise UnsupportedOperation.new('Unsupported operation') unless ALLOWED_OPERATIONS.include?(@operation)
+
+    return 'Division by zero is not allowed.' if @operand_2.zero?
+
+    result = @operand_1.send(@operation, @operand_2)
+
+    DISPLAY % {operand_1: @operand_1, operand_2: @operand_2, operation: @operation, result: result}
+  end
+
 end
 
 class SimpleCalculator::UnsupportedOperation < StandardError; end
+
+class OperandError < ArgumentError
+  def initialize(message= 'Operand must be Numeric')
+    super
+  end
+end
 
