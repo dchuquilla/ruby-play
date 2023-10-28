cd48004594d180e9fa0f2b546ece980a4bde5f26
dchuquilla <dario.chuquilla@gmail.com>
2023-10-28 08:56:03 -0500

    ✨ Add diamond project
    📊 List of changes made in bullet points:
    - 🌟 Enhancement:
      - [ ] Add diamond project files
    
    🔗 Related Issues:
    - None
    
    👥 Reviewers:
    - None

---
+++ 08-excercism/diamond/diamond.rb
@@ -0,0 +1,15 @@
+module Diamond
+  class << self
+    private
+    attr_accessor :letter, :heigth
+
+    def letter_index(letter)
+
+    end
+
+    public
+    def make_diamond(letter)
+
+    end
+  end
+end
