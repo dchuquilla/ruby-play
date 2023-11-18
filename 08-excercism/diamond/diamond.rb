75485bd3bd5486cea338069a64a0d5ff961334af
dchuquilla <dario.chuquilla@gmail.com>
2023-11-18 10:43:33 -0500

    ✨ IDX as ternary
    📊 List of changes made in bullet points:
    - 🌟 Enhancement:
      - [ ] Conget if else statement as ternary
    
    🔗 Related Issues:
    - None
    
    👥 Reviewers:
    - None

--- 08-excercism/diamond/diamond.rb
+++ 08-excercism/diamond/diamond.rb
@@ -2,7 +2,6 @@ module Diamond
   class << self
     private
     INDEX = 64
-    attr_accessor :letter, :heigth
 
     def letter_index(letter)
       letter.upcase.ord - INDEX
@@ -20,11 +19,7 @@ module Diamond
       secuence = idx.downto(1).to_a + 2.upto(idx).to_a
       secuence.size.times.each do |i|
         diamond += secuence.map { |j| j == line ? index_letter(j) : ' '}.join('') + "\n"
-        if i >= (idx - 1)
-          line -= 1
-        else
-          line += 1
-        end
+        i >= (idx - 1) ? line -= 1 : line += 1
       end
       diamond
     end
