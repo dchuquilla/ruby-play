bd538092c843fe6b1b88a1b3b5371f29f001e23f
dchuquilla <dario.chuquilla@gmail.com>
2023-11-25 09:51:26 -0500

    ✨ First solution to acronym
    📊 List of changes made in bullet points:
    - 🌟 Enhancement:
      - [ ] Create class with design and encapsulation
    
    🔗 Related Issues:
    - None
    
    👥 Reviewers:
    - None

--- 08-excercism/acronym/acronym.rb
+++ 08-excercism/acronym/acronym.rb
@@ -5,3 +5,26 @@ Write your code for the 'Acronym' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/acronym` directory.
 =end
+
+class Acronym
+  private
+
+  HYPENS = /-/
+  NOT_CHARS = /[^a-zA-Z]/
+
+  attr_reader :jargon
+
+  def initialize(prhase)
+    @jargon = prhase.gsub(HYPENS, ' ').split.map { |word| word[0].gsub(NOT_CHARS, '').upcase }.join
+  end
+
+  public
+
+  def to_s
+    @jargon
+  end
+
+  def self.abbreviate(prhase)
+    new(prhase).to_s
+  end
+end
