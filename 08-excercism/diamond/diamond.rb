b0628fcd01e4344bdfcdfc6a408dadca3f15dcca
dchuquilla <dario.chuquilla@gmail.com>
2023-12-17 15:48:00 -0500

    - 🌟 Enhancement:   - [ ] Simplify output with to_s   - [ ] Rename constant to be readable   - [ ] Correct order of elements
    👥 Reviewers:
    - KOTP

--- 08-excercism/diamond/diamond.rb
+++ 08-excercism/diamond/diamond.rb
@@ -1,36 +1,31 @@
 class Diamond
+
+  def self.make_diamond(letter)
+    new(letter).to_s
+  end
+
+  ASCII_A = 64
+
+  attr_reader :to_s
+
   private
 
-  CHAR_INDEX = 64
-
-  attr_reader :diamond_out
-
   def initialize(letter)
-    @diamond_out = ''
+    @to_s = ''
     index = letter_index(letter)
     line = 1
     sequence = index.downto(1).to_a + 2.upto(index).to_a
     sequence.size.times do |i|
-      @diamond_out += sequence.map { |j| j == line ? index_letter(j) : ' '}.join('') + "\n"
+      @to_s += sequence.map { |j| j == line ? index_letter(j) : ' '}.join('') + "\n"
       i >= (index - 1) ? line -= 1 : line += 1
     end
   end
 
   def letter_index(letter)
-    letter.upcase.ord - CHAR_INDEX
+    letter.upcase.ord - ASCII_A
   end
 
   def index_letter(index)
-    (index + CHAR_INDEX).chr
-  end
-
-  public
-
-  def to_s
-    @diamond_out
-  end
-
-  def self.make_diamond(letter)
-    new(letter).to_s
+    (index + ASCII_A).chr
   end
 end
