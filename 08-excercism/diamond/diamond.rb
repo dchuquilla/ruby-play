8feb7550aea741a9abccf397d270ecb551447169
dchuquilla <dario.chuquilla@gmail.com>
2024-01-30 09:24:19 -0500

    ✨ 7th iteration
    EOL fixed
    
    ___
    Added StringEolRefinement to refine string only in this context.
    
    ___
    Method index renamed to index_number
    
    ___
    ASCII_A made as private
    
    ___
    each_with_object('') used in line 40, now I learn that str act as accumulator of string.
    
    ___
    Thanks so much about all your comments
    
    👥 Reviewers:
    - None

--- 08-excercism/diamond/diamond.rb
+++ 08-excercism/diamond/diamond.rb
@@ -1,31 +1,56 @@
+module StringEolRefinement
+  refine String do
+    def eol!
+      self << "\n"
+    end
+  end
+end
+
 class Diamond
+  using StringEolRefinement
 
   def self.make_diamond(letter)
     new(letter).to_s
   end
 
-  ASCII_A = 64
-
-  attr_reader :to_s
+  attr_reader :to_s, :sequence, :index
 
   private
 
+  ASCII_A = 64
+  private_constant :ASCII_A
+
   def initialize(letter)
-    @to_s = ''
-    index = letter_index(letter)
-    line = 1
-    sequence = index.downto(1).to_a + 2.upto(index).to_a
+    @to_s = ""
+    diamond_row = 1
+    @index = index_number(letter)
+    @sequence = generate_sequence
+
     sequence.size.times do |i|
-      @to_s += sequence.map { |j| j == line ? index_letter(j) : ' '}.join('') + "\n"
-      i >= (index - 1) ? line -= 1 : line += 1
+      @to_s += generate_line(diamond_row)
+      # diamond_row will tell which number convert to letter
+      i >= (@index - 1) ? diamond_row -= 1 : diamond_row += 1
     end
   end
 
-  def letter_index(letter)
+  # use sequence to generate each line
+  # conver current row numbers to letter and fill in the line with spaces
+  # e.g. row 1 to A, row 2 to B, row 3 to C
+  def generate_line(row)
+    @sequence.each_with_object('') { |j, str| str << (j == row ? letter(j) : ' ') }.eol!
+  end
+
+  def index_number(letter)
     letter.upcase.ord - ASCII_A
   end
 
-  def index_letter(index)
-    (index + ASCII_A).chr
+  def letter(number)
+    (number + ASCII_A).chr
+  end
+
+  # generate sequence of count down and count up
+  # e.g. 3 => [3, 2, 1, 2, 3]
+  def generate_sequence
+    @index.downto(1).to_a + 2.upto(@index).to_a
   end
 end
