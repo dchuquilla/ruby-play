7e366293e75dd23babd1660083a9d6773eae81a5
dchuquilla <dario.chuquilla@gmail.com>
2024-02-18 11:57:18 -0500

    ✨ Diamond iteration 11 kotp
    Looking good.
    
    We can now use columns.with_object instead of columns.each_with_object, because Enumerator instances have a with_object method that can be used.
    
    We should probably have an instance variable for @columns, not @_columns, and the same for lines. Not sure why the name shift happened with the underscore. Can you explain? (I am sure it happened for a reason, I just do not know what that reason is.)
    
    We are at the point where we probably want to organize our code better.
    
    Remember that private does not affect the visibility of constants, only things at that same level of invocation of that method. So instance methods, but not constants, and not class level methods.
    
    And having the public depth and to_s is not necessary, we only need the public to_s method.
    
    But not only that, we currently define that method in two places. Remove the attr_reader :depth and then we will have only the one definition.
    
    We should use the methods we created, instead of using @depth, we have a method for that…
    
    I am looking forward to the next iteration!
    
    dchuquilla
    
    wow, with_object blows my mind.
    
    I use @_columns ||= as a memoize technique, it is used only ins the method and I do not want to re-calculate in all loop iterations. I tried runing the test 5k times for 'Z' letter, it is 1 sec slower without this.
    
    This applies also for @depth
    
    👥 Reviewers:
    - KOTP

--- 08-excercism/diamond/diamond.rb
+++ 08-excercism/diamond/diamond.rb
@@ -13,19 +13,20 @@ class Diamond
     new(letter).to_s
   end
 
-  attr_reader :to_s, :depth
+  attr_reader :to_s
 
   private
+  attr_reader :letter, :depth, :columns, :lines
 
   ASCII_A = 64
   private_constant :ASCII_A
 
   def initialize(letter)
     @to_s = ""
+    @letter = letter
     diamond_row = 1
-    @depth = depth_number(letter)
 
-    @to_s = lines.each_with_object('') do |vertical_point, line|
+    @to_s = lines.with_object('') do |vertical_point, line|
       line << generate_line(vertical_point)
     end
   end
@@ -34,14 +35,14 @@ class Diamond
   # conver current row numbers to letter and fill in the line with spaces
   # e.g. row 1 to A, row 2 to B, row 3 to C
   def generate_line(vertical_point)
-    columns.each_with_object('') do |horizontal_point, line|
+    columns.with_object('') do |horizontal_point, line|
       line << ' ' and next unless horizontal_point == vertical_point
       line << index_letter(horizontal_point)
     end.eol!
   end
 
-  def depth_number(letter)
-    letter.upcase.ord - ASCII_A
+  def depth_number
+    @depth ||= @letter.upcase.ord - ASCII_A
   end
 
   def index_letter(number)
@@ -51,12 +52,12 @@ class Diamond
   # generate sequence of count down and count up
   # e.g. 3 => [3, 2, 1, 2, 3]
   def columns
-    @_columns ||= (@depth.downto(1) + 2.upto(@depth)).to_enum
+    @columns ||= (depth_number.downto(1) + 2.upto(depth_number)).to_enum
   end
 
   # generate sequence of count up and count down
   # e.g. 3 => [1, 2, 3, 2, 1]
   def lines
-    @_lines ||= (1.upto(@depth) + (@depth - 1).downto(1)).to_enum
+    @lines ||= (1.upto(depth_number) + (depth_number - 1).downto(1)).to_enum
   end
 end
