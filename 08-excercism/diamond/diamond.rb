85c482b1c33ff58417b0ee7460c43a9835b8811c
dchuquilla <dario.chuquilla@gmail.com>
2024-02-24 18:04:48 -0500

    ✨ 14th iteration
    Can these memoized methods be done in initialize where we are meaning to initialize our internal state? Then the attribute_reader line can manage this method creation itself?
    
    And I think that will mean that we do not need the instance variable @letter at all.
    
    I had a hard time finding the public method to_s as it was above the private method initialize and that was below a class level item, the constant.
    
    Seems that the code is in a bit of disorganized state in terms of scope and even visibility.
    
    The private does not make the constant private, so we still have to do the statement three lines after. So moving that private down should be useful, bringing it closer to the things that it effects.

--- 08-excercism/diamond/diamond.rb
+++ 08-excercism/diamond/diamond.rb
@@ -9,19 +9,21 @@ end
 class Diamond
   using StringEolRefinement
 
-  def self.make_diamond(letter)
-    new(letter).to_s
-  end
-
   attr_reader :to_s
 
-  private
+  def self.make_diamond(letter)
+    d = new(letter)
+    d.to_s
+  end
 
+  private
   ASCII_A = 64
   private_constant :ASCII_A
 
   def initialize(letter)
-    @letter = letter
+    depth = letter.upcase.ord - ASCII_A
+    lines = (1.upto(depth) + (depth - 1).downto(1)).to_enum
+    @columns = (depth.downto(1) + 2.upto(depth)).to_enum
 
     @to_s = lines.with_object('') do |vertical_point, line|
       line << generate_line(vertical_point)
@@ -32,7 +34,7 @@ class Diamond
   # conver current row numbers to letter and fill in the line with spaces
   # e.g. row 1 to A, row 2 to B, row 3 to C
   def generate_line(vertical_point)
-    columns.with_object('') do |horizontal_point, line|
+    @columns.with_object('') do |horizontal_point, line|
       line << ' ' and next unless horizontal_point == vertical_point
       line << index_letter(horizontal_point)
     end.eol!
@@ -40,21 +42,5 @@ class Diamond
 
   def index_letter(number)
     (number + ASCII_A).chr
-  end
-
-  def depth
-    @depth ||= @letter.upcase.ord - ASCII_A
-  end
-
-  # generate sequence of count down and count up
-  # e.g. 3 => [3, 2, 1, 2, 3]
-  def columns
-    @columns ||= (depth.downto(1) + 2.upto(depth)).to_enum
-  end
-
-  # generate sequence of count up and count down
-  # e.g. 3 => [1, 2, 3, 2, 1]
-  def lines
-    @lines ||= (1.upto(depth) + (depth - 1).downto(1)).to_enum
   end
 end
