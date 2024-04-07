2301463a3bed76938d5b93da20c740d9d4a4db5e
dchuquilla <dario.chuquilla@gmail.com>
2024-04-06 19:10:40 -0500

    ✨ Add word wrapper challenge
    🔗 Related Issues:
    - None
    
    👥 Reviewers:
    - None

--- 03-challenges/word_wrapper.rb
+++ 03-challenges/word_wrapper.rb
@@ -5,17 +5,17 @@
 #
 input = "In 1991, while studying computer science at University of Helsinki, Linus Torvalds began a project that later became the Linux kernel. He wrote the program specifically for the hardware he was using and independent of an operating system because he wanted to use the functions of his new PC with an 80386 processor. Development was done on MINIX using the GNU C Compiler."
 
-output =
-"""In 1991, while studying computer science
-at University of Helsinki, Linus
-Torvalds began a project that later
-became the Linux kernel. He wrote the
-program specifically for the hardware he
-was using and independent of an
-operating system because he wanted to
-use the functions of his new PC with an
-80386 processor. Development was done on
-MINIX using the GNU C Compiler."""
+
+# """In 1991, while studying computer science
+# at University of Helsinki, Linus
+# Torvalds began a project that later
+# became the Linux kernel. He wrote the
+# program specifically for the hardware he
+# was using and independent of an
+# operating system because he wanted to
+# use the functions of his new PC with an
+# 80386 processor. Development was done on
+# MINIX using the GNU C Compiler."""
 
 def solution(text)
   #.text.scan(/\b.{1,40}\b/).map { |l| l.strip }.join("\n")
