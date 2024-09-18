c6abdcfd3731fd5d4bf6bd0176a3f079cfa6a396
dchuquilla <dario.chuquilla@gmail.com>
2024-09-18 10:59:31 -0500

    Refactor AssemblyLine class to include optional parameter for number of cars

--- 08-excercism/assembly-line/assembly_line.rb
+++ 08-excercism/assembly-line/assembly_line.rb
@@ -1,13 +1,23 @@
 class AssemblyLine
-  def initialize(speed)
+  def initialize(speed, cars = 221)
     @speed = speed
+    @cars = cars
   end
 
   def production_rate_per_hour
-    raise 'Please implement the AssemblyLine#production_rate_per_hour method'
+    rate = @speed * @cars
+    if @speed >= 1 && @speed <= 4
+      rate
+    elsif @speed > 4 && @speed <= 8
+      rate * 0.9
+    elsif @speed > 8 && @speed <= 9
+      rate * 0.8
+    else
+      rate * 0.77
+    end
   end
 
   def working_items_per_minute
-    raise 'Please implement the AssemblyLine#working_items_per_minute method'
+    (production_rate_per_hour / 60.0).floor
   end
 end
