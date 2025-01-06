78fbc72d4d83d9f59c5ced5cbe44ae4cffef457a
dchuquilla <dario.chuquilla@gmail.com>
2025-01-06 16:58:04 -0500

    Implement Isogram class with isogram? method and update tests

--- 08-excercism/isogram/isogram.rb
+++ 08-excercism/isogram/isogram.rb
@@ -5,3 +5,11 @@ Write your code for the 'Isogram' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/isogram` directory.
 =end
+class Isogram
+  def self.isogram?(input)
+    input.downcase!
+    input.gsub!(/[^a-z]/, "")
+    # input.split("").uniq.length == input.length
+    input.chars.uniq.length == input.length
+  end
+end
--- 08-excercism/isogram/isogram_test.rb
+++ 08-excercism/isogram/isogram_test.rb
@@ -1,5 +1,5 @@
-require 'minitest/autorun'
-require_relative 'isogram'
+require "minitest/autorun"
+require_relative "isogram"
 
 class IsogramTest < Minitest::Test
   def test_empty_string
@@ -9,79 +9,79 @@ class IsogramTest < Minitest::Test
   end
 
   def test_isogram_with_only_lower_case_characters
-    skip
+    # skip
     input = "isogram"
     assert Isogram.isogram?(input), "Expected true, '#{input}' is an isogram"
   end
 
   def test_word_with_one_duplicated_character
-    skip
+    # skip
     input = "eleven"
     refute Isogram.isogram?(input), "Expected false, '#{input}' is not an isogram"
   end
 
   def test_word_with_one_duplicated_character_from_the_end_of_the_alphabet
-    skip
+    # skip
     input = "zzyzx"
     refute Isogram.isogram?(input), "Expected false, '#{input}' is not an isogram"
   end
 
   def test_longest_reported_english_isogram
-    skip
+    # skip
     input = "subdermatoglyphic"
     assert Isogram.isogram?(input), "Expected true, '#{input}' is an isogram"
   end
 
   def test_word_with_duplicated_character_in_mixed_case
-    skip
+    # skip
     input = "Alphabet"
     refute Isogram.isogram?(input), "Expected false, '#{input}' is not an isogram"
   end
 
   def test_word_with_duplicated_character_in_mixed_case_lowercase_first
-    skip
+    # skip
     input = "alphAbet"
     refute Isogram.isogram?(input), "Expected false, '#{input}' is not an isogram"
   end
 
   def test_hypothetical_isogrammic_word_with_hyphen
-    skip
+    # skip
     input = "thumbscrew-japingly"
     assert Isogram.isogram?(input), "Expected true, '#{input}' is an isogram"
   end
 
   def test_hypothetical_word_with_duplicated_character_following_hyphen
-    skip
+    # skip
     input = "thumbscrew-jappingly"
     refute Isogram.isogram?(input), "Expected false, '#{input}' is not an isogram"
   end
 
   def test_isogram_with_duplicated_hyphen
-    skip
+    # skip
     input = "six-year-old"
     assert Isogram.isogram?(input), "Expected true, '#{input}' is an isogram"
   end
 
   def test_made_up_name_that_is_an_isogram
-    skip
+    # skip
     input = "Emily Jung Schwartzkopf"
     assert Isogram.isogram?(input), "Expected true, '#{input}' is an isogram"
   end
 
   def test_duplicated_character_in_the_middle
-    skip
+    # skip
     input = "accentor"
     refute Isogram.isogram?(input), "Expected false, '#{input}' is not an isogram"
   end
 
   def test_same_first_and_last_characters
-    skip
+    # skip
     input = "angola"
     refute Isogram.isogram?(input), "Expected false, '#{input}' is not an isogram"
   end
 
   def test_word_with_duplicated_character_and_with_two_hyphens
-    skip
+    # skip
     input = "up-to-date"
     refute Isogram.isogram?(input), "Expected false, '#{input}' is not an isogram"
   end
