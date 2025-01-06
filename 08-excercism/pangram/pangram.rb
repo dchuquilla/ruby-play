3c98053387a3e4a1e8508c9512d83640a3c70ff2
dchuquilla <dario.chuquilla@gmail.com>
2025-01-06 18:13:43 -0500

    Implement Pangram class with pangram? method to check if a sentence contains all letters of the alphabet

--- 08-excercism/pangram/pangram.rb
+++ 08-excercism/pangram/pangram.rb
@@ -5,3 +5,15 @@ Write your code for the 'Pangram' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/pangram` directory.
 =end
+
+class Pangram
+  def self.pangram?(sentence)
+    alphabet = ('a'..'z').to_a
+    sentence.downcase.chars.each do |char|
+      alphabet.delete(char) if alphabet.include?(char)
+      break if alphabet.empty?
+    end
+
+    alphabet.empty?
+  end
+end
